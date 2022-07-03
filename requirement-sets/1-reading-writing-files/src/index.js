22e5dfd28456db21381ebcde442c54de6dca61dd
AlexDjangoX <alexmonk17@gmail.com>
2022-07-03 16:35:56 +0200

    Solution 1

--- requirement-sets/1-reading-writing-files/src/index.js
+++ requirement-sets/1-reading-writing-files/src/index.js
@@ -1,1 +1,23 @@
-// run the program
+const fs = require('fs');
+
+function checkPosition(data) {
+  const answers = [];
+  const dataArray = data.toString().toUpperCase().split('\r\n');
+
+  dataArray.forEach((el) => {
+    const counts = {
+      W: 0,
+      E: 0,
+      N: 0,
+      S: 0,
+    };
+
+    el.split('').forEach((letter) => (counts[letter] += 1));
+
+    answers.push(counts.W === counts.E && counts.S === counts.N);
+  });
+  return answers;
+}
+
+const data = fs.readFileSync('../input.txt');
+console.log(checkPosition(data));
