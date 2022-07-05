270861054b0af1ef2b0412f6efe530e7ee8f4d04
Paulo Barata <pbara7a@gmail.com>
2022-07-05 14:33:54 +0100

    1-reading-writing-files

--- requirement-sets/1-reading-writing-files/src/index.js
+++ requirement-sets/1-reading-writing-files/src/index.js
@@ -1,1 +1,95 @@
-// run the program
+const fs = require("fs");
+
+const movementMap = { N: 1, S: -1, E: 1, W: -1 };
+
+// Main exercise
+let countStr = "Count: ";
+
+fs.readFile("./input.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const parsedLines = data.trim().split("\n");
+
+  let samePosCount = 0;
+  for (const line of parsedLines) {
+    const yAxisMovements = line
+      .split("")
+      .filter((step) => step === "N" || step === "S");
+
+    const xAxisMovements = line
+      .split("")
+      .filter((step) => step === "E" || step === "W");
+
+    let [yStepCount, xStepCount] = [0, 0];
+    for (const yStep of yAxisMovements) {
+      yStepCount += movementMap[yStep];
+    }
+
+    if (yStepCount !== 0) continue;
+
+    for (const xStep of xAxisMovements) {
+      xStepCount += movementMap[xStep];
+    }
+
+    if (xStepCount !== 0) continue;
+
+    samePosCount++;
+  }
+
+  countStr += samePosCount;
+
+  fs.writeFile("./answer.txt", countStr, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log("Done!");
+  });
+});
+
+// EXTENSION
+
+// let boolStr;
+
+// fs.readFile("./input.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+
+//   const parsedLines = data.trim().split("\n");
+
+//   for (const line of parsedLines) {
+//     const yAxisMovements = line
+//       .split("")
+//       .filter((step) => step === "N" || step === "S");
+
+//     const xAxisMovements = line
+//       .split("")
+//       .filter((step) => step === "E" || step === "W");
+
+//     let [yStepCount, xStepCount] = [0, 0];
+//     for (const yStep of yAxisMovements) {
+//       yStepCount += movementMap[yStep];
+//     }
+
+//     for (const xStep of xAxisMovements) {
+//       xStepCount += movementMap[xStep];
+//     }
+
+//     if (xStepCount === 0 && yStepCount === 0) boolStr = "true";
+//   }
+
+//   fs.writeFile("./answer.txt", boolStr, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     console.log("Done!");
+//   });
+// });
