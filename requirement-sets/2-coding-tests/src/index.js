d277c9fdb9a27184eb234fc49174d665c7fa7138
mbalabanov <marincomics@gmail.com>
2022-07-12 13:02:22 +0300

    Adds Jasmine tests

--- requirement-sets/2-coding-tests/src/index.js
+++ requirement-sets/2-coding-tests/src/index.js
@@ -1,63 +1,75 @@
 // run the program
 
-const input = "zzzzzzzzaaaatddddbhhhvvvaaaavvkkoooooooooonnnnnnnnnnmmmmmmmmmmiuhjkciiiiii"
+const oneCharNonRepeated = 'z'
+const oneCharRepeated = 'dddddddddd'
+const multiCharRepeatedUnique = 'zzzzzzzzaaaatddddbhhhvvv'
+const multiCharRepeatedNonUnique = 'zzzzzzzzaaaatddddbhhhvvvaaaavvkkoooooooooonnnnnnnnnnmmmmmmmmmmiuhjkciiiiii'
 
-function find_longest_sequence(inputString) {
-    let allDuplicates = []
-    let singleCharacterDuplicates = {}
+function find_longest_sequence (inputString) {
+  const allDuplicates = []
+  let singleCharacterDuplicates = {}
 
-    for (let charIndex = 0; charIndex < inputString.length; charIndex++) {
-        let thisCharacter = inputString[charIndex]
-        let nextCharacter
+  for (let charIndex = 0; charIndex < inputString.length; charIndex++) {
+    const thisCharacter = inputString[charIndex]
+    let nextCharacter
 
-        if (inputString[charIndex + 1]) {
-            nextCharacter = inputString[charIndex + 1]
-        }
-
-        if (singleCharacterDuplicates.char) {
-            singleCharacterDuplicates.amount++
-        } else {
-            singleCharacterDuplicates.char = thisCharacter
-            singleCharacterDuplicates.amount = 1
-        }
-
-        if (thisCharacter != nextCharacter) {
-            allDuplicates.push(singleCharacterDuplicates)
-            singleCharacterDuplicates = {}
-        }
-
+    if (inputString[charIndex + 1]) {
+      nextCharacter = inputString[charIndex + 1]
     }
-    results_sorted_by_value = allDuplicates.sort((a, b) => b.amount - a.amount)
-    unique_top_result = get_alphabetical_unique_top_result(results_sorted_by_value)[0]
-    
-    let output = {}
 
-    let resultKey = unique_top_result.char.toLowerCase()
-    let resultValue = unique_top_result.amount
+    if (singleCharacterDuplicates.char) {
+      singleCharacterDuplicates.amount++
+    } else {
+      singleCharacterDuplicates.char = thisCharacter
+      singleCharacterDuplicates.amount = 1
+    }
 
-    output[resultKey] = resultValue
-    
-    return output
+    if (thisCharacter !== nextCharacter) {
+      allDuplicates.push(singleCharacterDuplicates)
+      singleCharacterDuplicates = {}
+    }
+  }
+  let results_sorted_by_value = allDuplicates.sort((a, b) => b.amount - a.amount)
+  let unique_top_result = get_alphabetical_unique_top_result(results_sorted_by_value)[0]
+
+  const output = {}
+
+  const resultKey = unique_top_result.char.toLowerCase()
+  const resultValue = unique_top_result.amount
+
+  output[resultKey] = resultValue
+
+  return output
 }
 
-function get_alphabetical_unique_top_result(results) {
-    let top_results = []
+function get_alphabetical_unique_top_result (results) {
+  const top_results = []
 
-    for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
-        let thisEntry = results[resultIndex]
-        let nextEntry = { amount: 0 }
+  for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
+    const thisEntry = results[resultIndex]
+    let nextEntry = { amount: 0 }
 
-        top_results.push(thisEntry)
+    top_results.push(thisEntry)
 
-        if (results[resultIndex + 1]) {
-            nextEntry = results[resultIndex + 1]
-        }
-
-        if (thisEntry.amount !== nextEntry.amount) {
-            return top_results.sort((a, b) => a.char.localeCompare(b.char))
-        }
+    if (results[resultIndex + 1]) {
+      nextEntry = results[resultIndex + 1]
     }
+
+    if (thisEntry.amount !== nextEntry.amount) {
+      return top_results.sort((a, b) => a.char.localeCompare(b.char))
+    }
+  }
 }
 
-console.log(find_longest_sequence(input))
+const resultOneCharNonRepeated = find_longest_sequence(oneCharNonRepeated)
+const resultOneCharRepeated = find_longest_sequence(oneCharRepeated)
+const resultMultiCharRepeatedUnique = find_longest_sequence(multiCharRepeatedUnique)
+const resultMultiCharRepeatedNonUnique = find_longest_sequence(multiCharRepeatedNonUnique)
 
+// do not edit below this line
+module.exports = {
+  a: resultOneCharNonRepeated,
+  b: resultOneCharRepeated,
+  c: resultMultiCharRepeatedUnique,
+  d: resultMultiCharRepeatedNonUnique
+}
