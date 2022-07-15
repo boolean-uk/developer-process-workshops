f0ff3392f1599ef3384e9301170fdc8ad0cb67d1
mbalabanov <marincomics@gmail.com>
2022-07-15 19:25:17 +0300

    Adds solution for savings-account-balance

--- requirement-sets/2-coding-tests/spec/find-longest-sequence.spec.js
+++ requirement-sets/2-coding-tests/spec/find-longest-sequence.spec.js
@@ -1,19 +1,19 @@
 const { a, b, c, d } = require('../src/find-longest-sequence.js')
 
 describe(':', () => {
-    it('One character in input and not repeated', () => {
-        expect(a).toEqual({ z: 1 })
-    })
+  it('One character in input and not repeated', () => {
+    expect(a).toEqual({ z: 1 })
+  })
 
-    it('One character in input and repeated', () => {
-        expect(b).toEqual({ d: 10 })
-    })
+  it('One character in input and repeated', () => {
+    expect(b).toEqual({ d: 10 })
+  })
 
-    it('Multiple characters in input but only one with maximum count', () => {
-        expect(c).toEqual({ z: 8 })
-    })
+  it('Multiple characters in input but only one with maximum count', () => {
+    expect(c).toEqual({ z: 8 })
+  })
 
-    it('Multiple characters in input and many with maximum counts', () => {
-        expect(d).toEqual({ m: 10 })
-    })
+  it('Multiple characters in input and many with maximum counts', () => {
+    expect(d).toEqual({ m: 10 })
+  })
 })
