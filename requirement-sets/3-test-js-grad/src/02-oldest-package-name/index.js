31a13477039bc672d2cf7706f576294099071642
mbalabanov <marincomics@gmail.com>
2022-07-18 15:02:01 +0300

    Add exercise three without any solutions yet

--- requirement-sets/3-test-js-grad/src/02-oldest-package-name/index.js
+++ requirement-sets/3-test-js-grad/src/02-oldest-package-name/index.js
@@ -11,8 +11,32 @@ GET https://api.npms.io/v2/search/suggestions?q=react
 
 */
 
-module.exports = async function oldestPackageName() {
-  // TODO
+const axios = require('axios');
 
-  return name
-};
+axios
+  .get('https://api.npms.io/v2/search/suggestions?q=react')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// module.exports = async function oldestPackageName() {
+//   // TODO
+//   let response = await fetch('https://api.npms.io/v2/search/suggestions?q=react');
+
+//   console.log(response.status); // 200
+//   console.log(response.statusText); // OK
+//   console.log ('\n****Response: ****')
+//   console.log (response)
+
+//   if (response.status === 200) {
+//       let data = await response.text();
+//       // handle data
+//   }
+
+//   return name
+// };
+
