8036094944d406176f416b7b9eae736666cb43d9
AlexDjangoX <alexmonk17@gmail.com>
2022-07-20 18:20:15 +0200

    longest sequence spec

--- requirement-sets/2-coding-tests/.eslintrc.js
+++ requirement-sets/2-coding-tests/.eslintrc.js
@@ -2,14 +2,12 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
-    node: true
+    node: true,
+    jest: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  rules: {
-  }
+  rules: {}
 }
