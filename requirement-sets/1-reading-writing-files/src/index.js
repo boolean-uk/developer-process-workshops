ff1da7f90cd3f43a7ea261b319c1206ea6d3a3e0
AlexDjangoX <alexmonk17@gmail.com>
2022-07-20 14:35:14 +0200

    REDONE: 1-reading-writing-files

--- requirement-sets/1-reading-writing-files/src/index.js
+++ requirement-sets/1-reading-writing-files/src/index.js
@@ -1,61 +1,23 @@
 const fs = require('fs');
 
+const data = fs.readFileSync('../input.txt');
+const dataArray = data.toString().toUpperCase().trim().split('\r\n');
+console.log(dataArray);
+
 function checkPosition(data) {
-  const answers = [];
-  const dataArray = data.toString().toUpperCase().split('\r\n');
-
+  let count = 0;
   dataArray.forEach((el) => {
-    const counts = {
-      W: 0,
-      E: 0,
-      N: 0,
-      S: 0,
-    };
-
+    const counts = { W: 0, E: 0, N: 0, S: 0 };
     el.split('').forEach((letter) => (counts[letter] += 1));
-
-    answers.push(counts.W === counts.E && counts.S === counts.N);
+    if (counts.W === counts.E && counts.S === counts.N) count += 1;
   });
-  return answers;
+  const output = `count: ${count}`;
+  fs.writeFile('../answer.txt', output, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  return count;
 }
 
-const data = fs.readFileSync('../input.txt');
 console.log(checkPosition(data));
-
-const findOrCreateMaintainer = (maintainers, maintainer) => {
-  const foundMaintainer = maintainers.find((entry) => {
-    return entry.username === maintainer;
-  });
-
-  if (foundMaintainer) return foundMaintainer;
-
-  const createdMaintainer = { username: maintainer, packageNames: [] };
-  maintainers.push(createdMaintainer);
-  return createdMaintainer;
-};
-
-const addMaintainerForPackage = (maintainers, packageName, maintainer) => {
-  const packages = findOrCreateMaintainer(maintainers, maintainer).packageNames;
-  packages.push(packageName);
-};
-
-module.exports = async function organiseMaintainers() {
-  const { data } = await axios.get(
-    'https://api.npms.io/v2/search/suggestions?q=react'
-  );
-
-  console.log('DATA : ', JSON.stringify(data[0], null, 2));
-  let maintainers = [];
-
-  data.forEach((item) => {
-    let packageName = item.package.name;
-    item.package.maintainers.forEach((maintainer) => {
-      let username = maintainer.username;
-      addMaintainerForPackage(maintainers, packageName, username);
-    });
-    return maintainers;
-  });
-
-  // console.log(maintainers);
-  return maintainers;
-};
