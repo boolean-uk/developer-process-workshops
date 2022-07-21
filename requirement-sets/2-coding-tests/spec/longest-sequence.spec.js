4633a80123cbb86c661f2073f4749c026bf8d2bc
AlexDjangoX <alexmonk17@gmail.com>
2022-07-21 14:17:43 +0200

    longest-sequence-test-passing

--- requirement-sets/2-coding-tests/spec/longest-sequence.spec.js
+++ requirement-sets/2-coding-tests/spec/longest-sequence.spec.js
@@ -1,12 +1,21 @@
-const longestSequence = require('../src/longest-sequence.js')
+const longestSequence = require('../src/longest-sequence.js');
 
 describe('Longest sequence', () => {
-  it('sequence 1', () => {
-    expect(longestSequence('gggttrfdesdcxzzzzzzzzzz')).toEqual({ z: 10 })
-  })
-  it('sequence 2', () => {
-    expect(longestSequence('rrftghuuytredswwsdxxxxxxxxxxgftrgr')).toEqual({
-      x: 10
-    })
-  })
-})
+  it('scenario 1', () => {
+    expect(longestSequence('dghhhmhmx')).toEqual({ h: 3 });
+  });
+  it('scenario 2', () => {
+    expect(longestSequence('dhkkhhKKKt')).toEqual({ k: 3 });
+  });
+  it('scenario 3', () => {
+    expect(longestSequence('aBbBadddadd')).toEqual({ b: 3 });
+  });
+  it('scenario 4', () => {
+    expect(longestSequence('gggttrfdesdcxzzzzzzzzzz')).toEqual({ z: 10 });
+  });
+  it('scenario 5', () => {
+    expect(longestSequence('jshetdrefdddesawwesdcxzwwwwwwwwww')).toEqual({
+      w: 10,
+    });
+  });
+});
