d4de2500590414208a8f656c80241c085690b9d7
AlexDjangoX <alexmonk17@gmail.com>
2022-07-21 14:31:18 +0200

    longest-sequence-test-fail

--- requirement-sets/2-coding-tests/src/longest-sequence.js
+++ requirement-sets/2-coding-tests/src/longest-sequence.js
@@ -1,30 +1,45 @@
-const longestSequence = (str) => {
-  let longestSequence = { a: 0 }
-  let currentSequence = {}
+const longest_sequence = (str) => {
+  // the longest sequence
+  let longestSequence = { a: 0 };
+  // the current sequence
+  let currentSequence = {};
 
+  // loop
   for (let i = 0; i < str.length - 1; i++) {
-    const char = str[i].toLowerCase()
-    const nextChar = str[i + 1].toLowerCase()
+    // 1. find the current char
+    const char = str[i].toLowerCase();
+    // 2. find the next char
+    const nextChar = str[i + 1].toLowerCase();
 
+    // reset the current sequence
     if (!currentSequence[char]) {
-      currentSequence = {}
-      currentSequence[char] = 1
+      currentSequence = {};
+      currentSequence[char] = 1;
     }
 
+    // check the next char
     if (char === nextChar) {
-      currentSequence[char] += 1
-    }
-    if (Object.values(currentSequence)[0] > Object.values(longestSequence)[0]) {
-      longestSequence = { ...currentSequence }
-    } else if (
-      Object.values(currentSequence)[0] === Object.values(longestSequence)[0]
-    ) {
-      if (Object.keys(currentSequence)[0] < Object.keys(longestSequence)[0]) {
-        longestSequence = { ...currentSequence }
+      currentSequence[char] += 1;
+    } else {
+      // update the longest sequence if 1. is longer, 2. is the same and alphabetically earlier
+      // look at the values, compare them?
+      if (
+        Object.values(currentSequence)[0] > Object.values(longestSequence)[0]
+      ) {
+        // if the current sequence is longer, we update longest sequence
+        longestSequence = { ...currentSequence };
+        // 2. is the same and alphabetically earlier
+      } else if (
+        Object.values(currentSequence)[0] === Object.values(longestSequence)[0]
+      ) {
+        if (Object.keys(currentSequence)[0] < Object.keys(currentSequence)[0]) {
+          longestSequence = { ...currentSequence };
+        }
       }
     }
   }
-  return longestSequence
-}
 
-module.exports = longestSequence
+  return longestSequence;
+};
+
+module.exports = longest_sequence;
