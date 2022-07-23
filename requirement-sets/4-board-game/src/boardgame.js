1f8f5797c7d2bb547f64fcb54bc7cd1392c8ae1a
mbalabanov <marincomics@gmail.com>
2022-07-23 16:23:22 +0300

    Refactors to reduce methods and ignore invalid inputs

--- requirement-sets/4-board-game/src/boardgame.js
+++ requirement-sets/4-board-game/src/boardgame.js
@@ -1,104 +1,91 @@
 const BOARD_DIMENSIONS = {
-    xMaxPosition: 4,
-    yMaxPosition: 4
+  xMaxPosition: 4,
+  yMaxPosition: 4
 }
 
 const MOVEMENTS = {
-    N: {
-        xDiff: 0,
-        yDiff: 1
-    },
-    S: {
-        xDiff: 0,
-        yDiff: -1
-    },
-    E: {
-        xDiff: 1,
-        yDiff: 0
-    },
-    W: {
-        xDiff: -1,
-        yDiff: 0
-    }
+  N: {
+    xDiff: 0,
+    yDiff: 1
+  },
+  S: {
+    xDiff: 0,
+    yDiff: -1
+  },
+  E: {
+    xDiff: 1,
+    yDiff: 0
+  },
+  W: {
+    xDiff: -1,
+    yDiff: 0
+  }
 }
 
 const TURNS = {
-    L: {
-        N: 'W',
-        S: 'E',
-        E: 'N',
-        W: 'S'
-    },
-    R: {
-        N: 'E',
-        S: 'W',
-        E: 'S',
-        W: 'N'
-    }
+  L: {
+    N: 'W',
+    S: 'E',
+    E: 'N',
+    W: 'S'
+  },
+  R: {
+    N: 'E',
+    S: 'W',
+    E: 'S',
+    W: 'N'
+  }
 }
 
 class Player {
-    constructor(boardLimits) {
-        this.xPlayerPosition = 0
-        this.yPlayerPosition = 0
-        this.playerDirection = 'N'
-        this.movementLimits = boardLimits
+  constructor (boardLimits) {
+    this.xPlayerPosition = 0
+    this.yPlayerPosition = 0
+    this.playerDirection = 'N'
+    this.movementLimits = boardLimits
+  }
+
+  getPlayerData () {
+    return [
+      this.xPlayerPosition,
+      this.yPlayerPosition,
+      this.playerDirection
+    ]
+  }
+
+  setPlayerMoves (moves) {
+    const steps = moves.split('')
+
+    for (let step of steps) {
+      step = step.toUpperCase()
+
+      if (step === 'M') {
+        this.movePlayer()
+      }
+
+      if (step === 'L' || step === 'R') {
+        this.turnPlayer(step)
+      }
     }
+  }
 
-    getPlayerData() {
-        return [
-            this.xPlayerPosition,
-            this.yPlayerPosition,
-            this.playerDirection,
-        ]
+  movePlayer () {
+    if (this.xPlayerPosition + MOVEMENTS[this.playerDirection].xDiff > 0 &&
+        this.xPlayerPosition < this.movementLimits.xMaxPosition) {
+      this.xPlayerPosition += MOVEMENTS[this.playerDirection].xDiff
     }
-
-    setPlayerMoves(moves) {
-        const steps = moves.split('')
-
-        for (let step of steps) {
-            step = step.toUpperCase()
-
-            if (step === 'M') {
-                this.movePlayer()
-                continue
-            }
-
-            this.turnPlayer(step)            
-        }
-        this.checkForLowerLimits()
+    if (this.yPlayerPosition + MOVEMENTS[this.playerDirection].yDiff > 0 &&
+        this.yPlayerPosition < this.movementLimits.yMaxPosition) {
+      this.yPlayerPosition += MOVEMENTS[this.playerDirection].yDiff
     }
+  }
 
-    checkForLowerLimits() {
-        if (this.xPlayerPosition < 0) {
-            this.xPlayerPosition = 0
-        }
-        if (this.yPlayerPosition < 0) {
-            this.yPlayerPosition = 0
-        }
-    }
-
-    movePlayer() {
-        if (this.xPlayerPosition < this.movementLimits.xMaxPosition) {
-            this.xPlayerPosition += MOVEMENTS[this.playerDirection].xDiff
-        }
-        if (this.yPlayerPosition < this.movementLimits.yMaxPosition) {
-            this.yPlayerPosition += MOVEMENTS[this.playerDirection].yDiff
-        }
-    }
-
-    turnPlayer(turnDirection) {
-        this.playerDirection = TURNS[turnDirection][this.playerDirection]
-    }
-
+  turnPlayer (turnDirection) {
+    this.playerDirection = TURNS[turnDirection][this.playerDirection]
+  }
 }
 
-const playerThree = new Player(BOARD_DIMENSIONS)
-playerThree.movePlayer('RMMMMM')
-
-console.log(playerThree.getPlayerData())
-
 module.exports = {
-    BOARD_DIMENSIONS,
-    Player
+  BOARD_DIMENSIONS,
+  Player
 }
